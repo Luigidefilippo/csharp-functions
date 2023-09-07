726761a9ca265903c75989b7a4f6033a0995cd8e
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-07 15:24:37 +0200

    Function Print array

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNZIONI 
+static void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(","); 
+        }
+    }
+    Console.WriteLine("]");
+}
